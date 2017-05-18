4679a0178f4d34f516194748a23d282dc64eee9d
Eric Hartman <imerichartman@gmail.com>
2017-05-18 11:47:18 -0400

    create person object

--- hello_world/src/index.js
+++ hello_world/src/index.js
@@ -2,7 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Hello from './App';
 
+var person = {
+  personName: "Nick",
+  personAge: 25
+}
+
 ReactDOM.render(
-  <Hello name={"Nick"} age={24} />,
+  <Hello
+    name={person.personName}
+    age={person.personAge}
+  />,
   document.getElementById('root')
 );
